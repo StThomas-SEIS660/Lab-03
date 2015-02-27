f3e6ea015278ea7e72911c647c5110aa9a6f82a6
Chris Cauwels <beno6137@stthomas.edu>
2015-02-27 16:01:23 +0000

    script commit

---
+++ beno6137-Lab03.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Starter shell script
+# Put commands below
+# To run, type ./starter.sh (you need the "./")
+# It must have permissions starting with a "7"  
+mkdir A
+cd A
+cat > aa
+mkdir B
+cd B
+cat > bb
+cd ..
+mkdir C
+cd C
+cat > cc
+mkdir E
+cd E
+cat > ee
+cd ..
+mkdir F
+cd F
+cat > ff
+cd ..
+cd ..
+mkdir D
+cd D
+cat > dd
+mkdir G
+cd G
+cat > gg
+cd ..
+mkdir I
+cd I
+cat > ii
+cd ..
+mkdir H
+cd H
+cat > hh
+mkdir J
+cd J
+cat > jj
+cd ..
+mkdir K
+cd K
+cat > kk
+cd ..
+cd ..
+cd ..
