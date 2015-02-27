a41a5da91b082bd7f442873c7c7764af934bdd54
Chris Cauwels <beno6137@stthomas.edu>
2015-02-27 10:50:05 -0600

    third commit

--- beno6137-Lab03.sh
+++ beno6137-Lab03.sh
@@ -3,61 +3,11 @@
 # Put commands below
 # To run, type ./starter.sh (you need the "./")
 # It must have permissions starting with a "7"  
-mkdir main
-cd main
-mkdir A
-cd A
-cat > aa
-mkdir B
-cd B
-cat > bb
-cd ..
-mkdir C
-cd C
-cat > cc
-cd ..
-mkdir D
-cd D
-cat > dd
-cd ..
-cd ..
-mkdir E
-cd E
-cat > ee
-mkdir F
-cd F
-cat > ff
-cd ..
-mkdir G
-cd G
-cat > gg
-cd ..
-cd ..
-mkdir H
-cd H
-cat > hh
-mkdir I
-cd I
-cat > ii
-cd ..
-mkdir J
-cd J
-cat > jj
-cd ..
-mkdir K
-cd K
-cat > kk
-cd ..
-mkdir L
-cd L
-cat > ll
-cd ..
-cd ..
-mkdir M
-cd M
-cat > mm
-cd ..
-cd ..
-cd ..
-cd ..
+
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install tree
+apt-get install git
+
+
 
