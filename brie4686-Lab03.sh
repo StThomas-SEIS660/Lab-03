d88c8edb569712e314617c89c77411efe0b6c625
Neal Briest <brie4686@stthomas.edu>
2015-02-27 20:30:37 -0600

    supplementary commit

--- brie4686-Lab03.sh
+++ brie4686-Lab03.sh
@@ -5,6 +5,7 @@
 # It must have permissions starting with a "7"
 
 mkdir -p main/{A/{B,C,D},E/{F,G},H{I,J/{K,L},M}}
+touch -p main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 sudo apt-get install -y tree
 sudo apt-get install -y git
 
