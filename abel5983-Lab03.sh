4e1c1fa741cd4ac006db37072d8522bac702cbd7
vagrant <vagrant@base.calavera.biz>
2015-02-28 02:46:33 +0000

    script commit

---
+++ abel5983-Lab03.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Starter shell script
+# Put commands below
+# To run, type ./starter.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+mkdir A
+cd A
+mkdir B
+mkdir C
+mkdir D
+cd C
+mkdir E
+mkdir F
+cd ..
+cd D
+mkdir G
+mkdir H
+mkdir I
+cd H
+mkdir J
+mkdir K
+sudo apt-get install -y tree
+sudo apt-get install -y git
+sudo apt-get install -y vim
+mkdir -p  "boo"
