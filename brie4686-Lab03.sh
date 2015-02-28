f12314db6b6a5d79bdc14d909f9662efeb942020
vagrant <vagrant@precise64.(none)>
2015-02-28 01:45:07 +0000

    Lab 3 (brie4686): Vagrantfile

---
+++ brie4686-Lab03.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Starter shell script
+# Put commands below
+# To run, type ./starter.sh (you need the "./")
+# It must have permissions starting with a "7"
+
+mkdir -p main/{A/{B,C,D},E/{F,G},H{I,J/{K,L},M}}
+sudo apt-get install -y tree
+sudo apt-get install -y git
+
