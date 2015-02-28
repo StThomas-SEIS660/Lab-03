430b282b1f3d9f12cc29a26d7a2e5362a98cddaf
vagrant <vagrant@precise64.(none)>
2015-02-28 02:23:05 +0000

    Lab 3: Vagrantfile - added vim to VM deployment script

--- kars0011-Lab03.sh
+++ kars0011-Lab03.sh
@@ -10,3 +10,4 @@ mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
 touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 sudo apt-get install -y tree
 sudo apt-get install -y git
+sudo apt-get install -y vim
