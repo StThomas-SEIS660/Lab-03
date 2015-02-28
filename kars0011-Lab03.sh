8bbc283afd48168ca404aa8f7999fa8fe118bb5f
vagrant <vagrant@precise64.(none)>
2015-02-28 02:05:24 +0000

    Lab 3 (kars0011): pull request Vagrantfile

--- kars0011-Lab03.sh
+++ kars0011-Lab03.sh
@@ -1,7 +1,12 @@
+
 #!/bin/bash
 # Starter shell script
+# Rename as YourID-Lab03.sh
 # Put commands below
-# To run, type ./starter.sh (you need the "./")
-# It must have permissions starting with a "7"
+# To run, type ./YourID-Lab03.sh (you need the "./")
+# It must have permissions starting with a "7"  
 
-mkdir "boo"
+mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get install tree
+apt-get install git
