9ee4c501c15bc2354f6a24c85d8640f500344bef
buzztdenis <buzztdeni@gmail.com>
2015-03-06 22:26:18 +0000

    2nd script commit

--- baza9762-Lab03.sh
+++ baza9762-Lab03.sh
@@ -5,7 +5,8 @@
 # To run, type ./starter.sh (you need the "./")
 # It must have permissions starting with a "7"
 
-mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
-touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+  mkdir -p main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+  touch main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+
 sudo apt-get install tree
 sudo apt-get install git
